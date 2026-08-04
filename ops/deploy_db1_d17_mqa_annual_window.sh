4dd36bf3488cb3ee1723558d941dc9ed4fd30328
ChessLab <stevenmacgregor@gmail.com>
2026-08-04 14:55:13 +0100

    Fix D17 deployment wrapper

--- ops/deploy_db1_d17_mqa_annual_window.sh
+++ ops/deploy_db1_d17_mqa_annual_window.sh
@@ -5,16 +5,17 @@ trap 'rm -rf "$template_root"' EXIT
 git clone --depth 1 https://github.com/comparative-legislative-data/rebuild-legislative-data.git "$template_root/source"
 generated="$template_root/source/ops/.deploy_db1_d17_body.sh"
 sed \
-  -e 's/mqa-event-types/mqa-questions-2026/g' \
-  -e 's/mqa-event-links/votes-on-motions-2026/g' \
-  -e 's/gb-sct.mqa-event-types.collection/gb-sct.mqa-questions-2026.collection/g' \
-  -e 's/gb-sct.mqa-event-links.collection/gb-sct.votes-on-motions-2026.collection/g' \
-  -e 's/d13/d17/g' -e 's/D13/D17/g' \
-  -e 's/mqa_taxonomy_link/mqa_annual_window/g' -e 's/mqa-taxonomy-link/mqa-annual-window/g' \
-  -e 's/MQA taxonomy\/link/MQA annual-window/g' -e 's/MQA taxonomy\/link/MQA annual-window/g' \
-  -e 's/for timer in d4a d4c d5 d6 d7 d8 d9 d10 KEEP_ELEVEN d12; do/for timer in d4a d4c d5 d6 d7 d8 d9 d10 KEEP_ELEVEN d12 d13 d14 d15 d16; do/' \
-  -e 's/for timer in d4a d4c d5 d6 d7 d8 d9 d10 KEEP_ELEVEN d12 d13; do/for timer in d4a d4c d5 d6 d7 d8 d9 d10 KEEP_ELEVEN d12 d13 d14 d15 d16 d17; do/' \
+  -e '/  gb-sct.members.collection/,/  gb-sct.member-government-roles.collection/c\
+  gb-sct.mqa-questions-2026.collection\
+  gb-sct.votes-on-motions-2026.collection' \
+  -e 's/for timer in d4a d4c d5 d6 d7 d8 d9 d10; do/for timer in d4a d4c d5 d6 d7 d8 d9 d10 KEEP_ELEVEN d12 d13 d14 d15 d16; do/' \
+  -e 's/for timer in d4a d4c d5 d6 d7 d8 d9 d10 d11; do/for timer in d4a d4c d5 d6 d7 d8 d9 d10 KEEP_ELEVEN d12 d13 d14 d15 d16 d17; do/' \
+  -e 's/for path in members member-constituency-statuses member-region-statuses member-parties member-party-roles member-government-roles; do/for path in mqa-questions-2026 votes-on-motions-2026; do/' \
+  -e 's/\[\[ "\$release_count" == "6" \]\]/[[ "$release_count" == "2" ]]/' \
+  -e 's/d11/d17/g' -e 's/D11/D17/g' \
+  -e 's/member_context/mqa_annual_window/g' -e 's/member-context/mqa-annual-window/g' \
+  -e 's/Member-context/MQA annual-window/g' -e 's/Member context/MQA annual-window/g' \
   -e 's/KEEP_ELEVEN/d11/g' \
-  "$template_root/source/ops/deploy_db1_d13_mqa_taxonomy_link.sh" > "$generated"
+  "$template_root/source/ops/deploy_db1_d11_member_context.sh" > "$generated"
 chmod 0700 "$generated"
 "$generated" --from-clone
